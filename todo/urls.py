c412da93b1c2d53977d35defaaae9748d4725a92
suhailvs <suhailvs@gmail.com>
2014-10-25 07:32:45 +0530

    added todoapp

--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'todo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^todo/', include('todoapp.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
