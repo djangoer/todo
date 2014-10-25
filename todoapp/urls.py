aa101e05cbb8e3efac92f8a8e4077f41e761e50e
suhailvs <suhailvs@gmail.com>
2014-10-25 08:20:01 +0530

    added full calendar

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('todoapp.views',
 	url(r'^$', 'home', name='home'),
+	url(r'^calendar/$', 'calendar', name='calendar'),
 )
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request,'home.html')
+
+def calendar(request):
+	return render(request,'calendar.html')
